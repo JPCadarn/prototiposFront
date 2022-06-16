529b710156440618a54b92e4c8604085fb46ef2b
JP Mariotti <mariotti660@gmail.com>
2022-06-15 22:09:37 -0300

    feat(livros): Tela livros

--- trabalhoGui/assets/index.js
+++ trabalhoGui/assets/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $('.collapsible').collapsible();
 });
+
+$(document).ready(function(){
+    $('.modal').modal();
+});
+
+$(document).ready(function(){
+    $('.fixed-action-btn').floatingActionButton();
+});
